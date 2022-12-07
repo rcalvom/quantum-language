986156385bcc204c5bc13b94c1bcc3b7288c7824
Juan Camilo Acosta Rojas <juacostar@unal.edu.co>
2022-12-06 19:15:31 -0500

    adding shell command paramteres

--- manage.py
+++ manage.py
@@ -1,7 +1,16 @@
 """Manage File"""
+import qiskit
 
 # Quantum Language
 from interpreter.execution.interpret import interpret
 
+import sys
+
 if __name__ == '__main__':
-    interpret("code")
+    file = sys.argv[0]
+    ibm = sys.argv[1]
+    executedInQuantumComputer = False
+
+    if ibm == 'ibm':
+        executedInQuantumComputer = True
+    interpret(file)
