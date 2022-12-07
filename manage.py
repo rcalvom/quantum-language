033d8146c54c31400cf7ea9eed16fc7d6b550ee6
Ricardo Calvo <ricardo.calvo@fitideas.co>
2022-12-06 22:43:37 -0500

    Final7

--- manage.py
+++ manage.py
@@ -1,23 +1,10 @@
 """Manage File"""
-import qiskit
 
 # Python
-import os
 import sys
 
 # Quantum Language
 from interpreter.execution.interpret import interpret
 
-import sys
-
-# if __name__ == '__main__':
-#     file = sys.argv[0]
-#     ibm = sys.argv[1]
-#     executedInQuantumComputer = False
-#
-#     if ibm == 'ibm':
-#         executedInQuantumComputer = True
-#     interpret(file)
-#
-
-interpret('simon', False)
+if __name__ == '__main__':
+    interpret(sys.argv[1], False)
